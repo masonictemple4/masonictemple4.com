ffd6f75dd2c47a39b634abba464468382abf7f4a
mason tucker <mason@listella.com>
2023-06-05 09:37:21 -0400

    feat: initial commit.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'media.licdn.com',
+    ],
+  }
+}
 
 module.exports = nextConfig
